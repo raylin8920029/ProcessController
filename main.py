42c6c1df0bd6e3c99181fdb77d8af78c07458bc6
freeman <free8920029@gmail.com>
2017-10-16 18:18:15 +0800

    Apply psutil (python system and process utilities).

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-import os
-import subprocess
+import psutil
 
 
 class ProcessController(object):
     def __init__(self):
         # The index of first process data
-        self.TASKLIST_PROCESS_INFO_START_INDEX = 3
-
-        # The list of process
-        self.process_list = []
-
-    def kill_process_by_name(process_name):
-        try:
-            killed = os.system('taskkill /fi "WINDOWTITLE eq %s"' % process_name)
-        except Exception, e:
-            killed = 0
-        return killed
+        self.process_info_list = []
 
     @staticmethod
-    def list_all_process_info():
-        try:
-            print subprocess.check_output('tasklist')
-        except subprocess.CalledProcessError as grepexc:
-            print "Called Process Error Exception Is Raised!!"
-            print "Error code :", grepexc.returncode, grepexc.output
-        except Exception:
-            print "Unexpected Exception Is Raised!!"
+    def list_all_process_info_by_tasklist():
+        for proc in psutil.process_iter():
+            try:
+                pinfo = proc.as_dict(attrs=['pid', 'name'])
+            except psutil.NoSuchProcess:
+                pass
+            else:
+                print(pinfo)
 
-    def __create_process_list(self):
-        self.process_list = subprocess.check_output('tasklist').split('\n')
-        print self.process_list[self.TASKLIST_PROCESS_INFO_START_INDEX]
+    def kill_process_by_name(self, process_name):
+        print "Input Process Name : %s" % process_name
+        print "================== Stopped Process List =================="
+
+        self.__create_process_info_list()
+        process_counter = 0
+
+        for process_info in self.process_info_list:
+            if process_name in process_info[0]:
+                process_counter += 1
+                print "%s. Stop Process : %s \n" % (str(process_counter), process_info[0])
+                process = psutil.Process(process_info[1])
+                process.terminate()
+
+    def __create_process_info_list(self):
+        for proc in psutil.process_iter():
+            try:
+                pinfo = proc.as_dict(attrs=['pid', 'name'])
+            except psutil.NoSuchProcess:
+                pass
+            else:
+                self.process_info_list.append([pinfo['name'], pinfo['pid']])
 
 
 def main():
     control = ProcessController()
-    control.list_all_process_info()
+    control.kill_process_by_name("notepad")
 
 if __name__ == "__main__":
     main()
